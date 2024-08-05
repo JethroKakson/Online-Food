d27f29763c3e9c82c7b578e0538cd6ce10ac30bc
JethroKakson <jethrokakson@gmail.com>
2024-08-05 13:41:22 +0300

    The first commit

---
+++ registration.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+session_start(); 
+error_reporting(0); 
+include("connection/connect.php"); 
+if(isset($_POST['submit'] )) 
+{
+     if(
+		empty($_POST['password'])||
+		empty($_POST['cpassword']) ||
+		empty($_POST['cpassword']))
+		{
+			$message = "All fields must be Required!";
+		}
+	else
+	{
+	
+	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
+	// $check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
+		
+
+	
+	if($_POST['password'] != $_POST['cpassword']){  
+       	
+          echo "<script>alert('Password not match');</script>"; 
+    }
+	elseif(strlen($_POST['password']) < 6)  
+	{
+      echo "<script>alert('Password Must be >=6');</script>"; 
+	}
+	
+	elseif(mysqli_num_rows($check_username) > 0) 
+     {
+       echo "<script>alert('Username Already exists!');</script>"; 
+     }
+	else{
+       
+	 
+	$mql = "INSERT INTO users(username,password) VALUES('".$_POST['username']."','".md5($_POST['password'])."')";
+	mysqli_query($db, $mql);
+	
+		 header("refresh:0.1;url=./admin/dashboard.php");
+    }
+	}
+
+}
+
+
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Table registration</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+<div>
+         <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+               <div class="container">
+                  <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                  <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-mania-logo.png" alt=""> </a>
+                  <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                     <ul class="nav navbar-nav">
+							<!-- <li class="nav-item"> <a class="nav-link active" href="home.php">Home <span class="sr-only">(current)</span></a> </li> -->
+                            <li class="nav-item"> <a class="nav-link active" href="./admin/dashboard.php">Dashboard <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								//echo '<li class="nav-item"><a href="index.php" class="nav-link active">Login</a> </li>';
+							//   <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+									
+									
+										//echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									//echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                  </div>
+               </div>
+            </nav>
+         </header>
+         <div class="page-wrapper">
+            
+               <div class="container">
+                  <ul>
+                    
+                    
+                  </ul>
+               </div>
+            
+            <section class="contact-page inner-page">
+               <div class="container ">
+                  <div class="row ">
+                     <div class="col-md-12">
+                        <div class="widget" >
+                           <div class="widget-body">
+                            
+							  <form action="" method="post">
+                                 <div class="row">
+								  <div class="form-group col-sm-12">
+                                       <label for="exampleInputEmail1">Table-Name</label>
+                                       <input class="form-control" type="text" name="username" id="example-text-input"> 
+                                    </div>
+                                    
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">Password</label>
+                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1"> 
+                                    </div> 
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">Confirm password</label>
+                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2"> 
+                                    </div>
+									 
+                                   
+                                 </div>
+                                
+                                 <div class="row">
+                                    <div class="col-sm-4">
+                                       <p> <input type="submit" value="Add table" name="submit" class="btn theme-btn"> </p>
+                                    </div>
+                                 </div>
+                              </form>
+                  
+						   </div>
+           
+                        </div>
+                     
+                     </div>
+                    
+                  </div>
+               </div>
+            </section>
+            
+      
+            <footer>
+               <p>&copy; 2024 - My Foods</p>
+            </footer>
+         
+         </div>
+       
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
